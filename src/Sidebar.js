ce14db3ed7ef3ee1c738e2ffb56ffd99fda7ba50
Rishi Raam N S <rishiraamns@gmail.com>
2023-02-12 10:33:28 +0530

    Added Sidebar Header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,24 @@
+import { Chat, DonutLarge, MoreVert } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
     return <div className="sidebar">
-        <h1>I am sidebar</h1>
+        <div className="sidebar__header">
+            <Avatar src="https://avatars.githubusercontent.com/u/76812006?v=4"/>
+            <div className="sidebar__headerRight">
+                <IconButton>
+                    <DonutLarge />
+                </IconButton>
+                <IconButton>
+                    <Chat />
+                </IconButton>
+                <IconButton>
+                    <MoreVert/> 
+                </IconButton>
+            </div>
+        </div>
     </div>
 }
 
