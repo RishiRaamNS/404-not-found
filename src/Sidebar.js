f6543ede0db1698c98e9ee6aee0d4c0d593d95d1
Rishi Raam N S <rishiraamns@gmail.com>
2023-02-12 11:02:16 +0530

    Completed the Sidebar Section

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,8 @@
-import { Chat, DonutLarge, MoreVert } from "@mui/icons-material";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@mui/icons-material";
 import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat"
 
 function Sidebar() {
     return <div className="sidebar">
@@ -18,6 +19,19 @@ function Sidebar() {
                     <MoreVert/> 
                 </IconButton>
             </div>
+        </div>
+
+        <div className="sidebar__search">
+            <div className="sidebar__searchContainer">
+                <SearchOutlined/>
+                <input placeholder="Search or start new chat" type="text"/>
+            </div>
+        </div>
+
+        <div className="sidebar__chats">
+            <SidebarChat />
+            <SidebarChat/>
+            <SidebarChat/>
         </div>
     </div>
 }
